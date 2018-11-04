9a0055dec132207372445c218d3fabbe0a1366cd
Patrick Skiba <skibap@my.easternct.edu>
2018-11-04 01:31:27 -0400

    seems to be working yay

--- main.py
+++ main.py
@@ -9,19 +9,31 @@ pacman_progs = {k for k, v in programs.items() if v["source"] == "pacman"}
 
 aur_progs = {k for k, v in programs.items() if v["source"] == "aur"}
 
-commands = [
-    f'sudo pacman -S --needed --noconfirm {*pacman_progs}',
-    'git clone https://aur.archlinux.org/package-query.git',
-    'cd package-query',
-    'makepkg -si',
-    'cd -',
-    'git clone https://aur.archlinux.org/yaourt.git',
-    'cd  yaourt',
-    'makepkg -si',
-    'cd -',
-    'sudo rm -dR yaourt package-query',
-    'sudo pacman -Sy yaourt',
-    f'yaourt -S --needed --noconfirm {*aur_progs}',
-    ]
+install_yaourt = [
+        'git clone https://aur.archlinux.org/package-query.git',
+        'cd package-query',
+        'makepkg -si',
+        'cd ../',
+        'git clone https://aur.archlinux.org/yaourt.git',
+        'cd yaourt',
+        'makepkg -si',
+        'cd ../',
+        'sudo rm -dR yaourt package-query',
+        'sudo pacman -Sy yaourt',
+        ]
 
-run_cmd(commands)
+install_dotfiles = [
+        'cd ~/'
+        'git init'
+        'git remote add origin https://github.com/Patrickskiba/dotfiles',
+        'git fetch',
+        'git checkout -t origin/master'
+        ]
+
+run(['sudo', 'pacman', '-S', '--needed', '--noconfirm', *pacman_progs])
+
+run_cmd(install_yaourt)
+
+run(['yaourt', '-S', '--needed', '--noconfirm', *aur_progs])
+
+run_cmd(install_dotfiles)
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+from os import chdir
 from subprocess import run
 
 def run_cmd(commands):
     for cmd in commands:
         split_cmd = [ x for x in cmd.split(' ') ]
-        run(split_cmd)
+        print(split_cmd[0])
+        if split_cmd[0] == 'cd':
+            print(split_cmd[1])
+            chdir(split_cmd[1])
+            run(['pwd'])
+        else:
+            run(split_cmd)
+
